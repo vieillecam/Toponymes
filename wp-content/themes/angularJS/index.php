7b7434447f7045225c1d617d172d2d97f07f5aba
Camille <vieillecam@free.fr>
2015-03-30 11:53:46 -0400

    ajout logo dans la Navbar

--- wp-content/themes/angularJS/index.php
+++ wp-content/themes/angularJS/index.php
@@ -1,43 +1,22 @@
 <?php get_header(); ?>
 
-<!-- <div ng-controller="mycontroller">
-  <div>{{sitedata.name}}</div>
-  <div>{{sitedata.description}}</div>
-  <ul>
-    <li ng-repeat="post in postdata">{{post.title}}
-	{{post.content}}
-    </li>
-  </ul>
-</div> -->
-
-<!-- <div ng-controller="AlertDemoCtrl">
-  <alert ng-repeat="alert in alerts" type="{{alert.type}}" close="closeAlert($index)">{{alert.msg}}</alert>
-  <button class='btn btn-default' ng-click="addAlert()">Add Alert</button>
-</div> -->
-
-<!-- <div ng-controller="mycontroller">
-	<leaflet id="map" center:"center"></leaflet>
-</div> -->
-<!--     <form>
-        Latitude : <input type="number" step="any" ng-model="center.lat">
-        Longitude : <input type="number" step="any" ng-model="center.lng">
-        Zoom : <input type="number" step="any" ng-model="center.zoom">
-    </form> -->
 <div class="container-fluid">
 	<nav class="navbar navbar-inverse" id="nav">
-	    <!-- Brand and toggle get grouped for better mobile display -->
-	    <div class="navbar-header">
-	      <a class="navbar-brand" href="#">Toponymie de fantaisie</a>
+	    <div class="container-fluid">
+		    <div class="navbar-header">
+		      <a class="navbar-brand" href="#">
+		      <img alt="Brand" src="http://localhost/img/icon.png" style="max-height: 40px; display:inline;">
+		      Toponymie de fantaisie
+		      </a>
+		    </div>
+
+		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		      <ul class="nav navbar-nav">
+		        <li class="active"><a href="#">Montmagny <span class="sr-only">(current)</span></a></li>
+		      </ul>
+		    </div>
 	    </div>
-
-	    <!-- Collect the nav links, forms, and other content for toggling -->
-	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-	      <ul class="nav navbar-nav">
-	        <li class="active"><a href="#">Montmagny <span class="sr-only">(current)</span></a></li>
-	      </ul>
-	    </div><!-- /.navbar-collapse -->
 	</nav>
-
 
 	<div ng-controller="mycontroller" class="row">	
 		<div id="map" class="col-lg-8">
