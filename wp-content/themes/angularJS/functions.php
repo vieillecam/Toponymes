4bb254a12fdefb47a0ff6470afc110c923ccc17c
Camille <vieillecam@free.fr>
2015-04-06 10:18:50 -0400

    regorganisation librairies avec bower

--- wp-content/themes/angularJS/functions.php
+++ wp-content/themes/angularJS/functions.php
@@ -2,22 +2,22 @@
 function mytheme_enqueue_scripts() {
   
   // register AngularJS
-  wp_register_script('jQuery', '//code.jquery.com/jquery-1.11.2.min.js', array(), null, false);
-  wp_register_script('angular-core', '//ajax.googleapis.com/ajax/libs/angularjs/1.2.28/angular.min.js', array(), null, false);
+  wp_register_script('jQuery', get_bloginfo('template_directory').'/bower_components/jquery/dist/jquery.min.js', array(), null, false);
+  wp_register_script('angular-core', get_bloginfo('template_directory').'/bower_components/angular/angular.min.js', array(), null, false);
   //  register the AngularUI Bootstraped js file
-  wp_register_script('angular-ui', "//angular-ui.github.io/bootstrap/ui-bootstrap-tpls-0.12.0.js", array(), null, false);
-  wp_register_script('leaflet-core', "//cdn.leafletjs.com/leaflet-0.7.1/leaflet.js", array(), null, false);
-  wp_register_script('leaflet-Angular', get_bloginfo('template_directory')."/angular-leaflet-directive.min.js", array(), null, false);
-  wp_register_script('SemanticUI', get_bloginfo('template_directory')."/semantic.min.js", array(), null, false);
+  wp_register_script('angular-ui', get_bloginfo('template_directory').'/bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js', array(), null, false);
+  wp_register_script('leaflet-core', get_bloginfo('template_directory').'/bower_components/leaflet/dist/leaflet.js', array(), null, false);
+  wp_register_script('leaflet-Angular', get_bloginfo('template_directory').'/bower_components/angular-leaflet-directive/dist/angular-leaflet-directive.min.js', array(), null, false);
+  wp_register_script('SemanticUI', get_bloginfo('template_directory').'/bower_components/semantic/dist/semantic.min.js', array(), null, false);
   wp_register_script('angular-animate', get_bloginfo('template_directory').'/bower_components/angular-animate/angular-animate.min.js', array(), null, false);
   wp_register_script('angular-aria', get_bloginfo('template_directory').'/bower_components/angular-aria/angular-aria.min.js', array(), null, false);
   wp_register_script('angular-material', get_bloginfo('template_directory').'/bower_components/angular-material/angular-material.min.js', array(), null, false);
   wp_register_script('My-app', get_bloginfo('template_directory').'/app.js', array('angular-core'), null, false);
 
   // on ajoute les fichiers CSS
-  wp_register_style( 'bootstrap-css', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css', array(), null, false );
-  wp_register_style( 'leaflet-css', '//cdn.leafletjs.com/leaflet-0.7.1/leaflet.css', array(), null, false );
-  wp_register_style( 'semanticCSS', get_bloginfo('template_directory')."/semantic.min.css", array(), null, false );
+  wp_register_style( 'bootstrap-css', get_bloginfo('template_directory').'/bower_components/bootstrap/dist/css/bootstrap.min.css', array(), null, false );
+  wp_register_style( 'leaflet-css', get_bloginfo('template_directory').'/bower_components/leaflet/dist/leaflet.css', array(), null, false );
+  wp_register_style( 'semanticCSS', get_bloginfo('template_directory').'/bower_components/semantic/dist/semantic.min.css', array(), null, false );
   wp_register_style( 'angular-material-css', get_bloginfo('template_directory')."/bower_components/angular-material/angular-material.min.css", array(), null, false );
   wp_register_style( 'my-css', get_bloginfo('template_directory')."/style.css", array(), null, false );
 
